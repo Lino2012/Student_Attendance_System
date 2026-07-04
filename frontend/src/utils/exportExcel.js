2e017da0929fc553ca184267e4280114f4f95312
Adithya Rajan <adithyarajan2k27@gmail.com>
2026-07-04 23:39:40 +0530

    fix:deleted all the files

--- frontend/src/utils/exportExcel.js
+++
@@ -1,9 +0,0 @@
-import * as XLSX from 'xlsx'
-
-export function exportToExcel({ columns, rows, filename = 'report.xlsx', sheetName = 'Report' }) {
-  const data = [columns, ...rows]
-  const worksheet = XLSX.utils.aoa_to_sheet(data)
-  const workbook = XLSX.utils.book_new()
-  XLSX.utils.book_append_sheet(workbook, worksheet, sheetName)
-  XLSX.writeFile(workbook, filename)
-}
